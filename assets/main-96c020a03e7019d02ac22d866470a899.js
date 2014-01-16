4450558a3c3ef0337395e9e70907a42a3a1a34a3
Ryan Fisher <ryancfisher@gmail.com>
2014-01-15 23:00:44 -0800

    Site updated at 2014-01-16 07:00:44 UTC

--- assets/main-96c020a03e7019d02ac22d866470a899.js
+++ assets/main-96c020a03e7019d02ac22d866470a899.js
@@ -2,7 +2,7 @@
   $(document).on('ready', function() {
     var nav_buttons;
     nav_buttons = $('nav a');
-    return nav_buttons.click(function() {
+    nav_buttons.click(function() {
       var callback, page;
       nav_buttons.removeClass('current');
       page = $(event.currentTarget).addClass('current').data('page');
@@ -16,6 +16,14 @@
       } else {
         return $('.details').slideUp(200, callback);
       }
+    });
+    return $('form').submit(function() {
+      var song;
+      event.preventDefault();
+      song = $('input:text').val();
+      ga('send', 'event', 'Song Recommendation', song);
+      $('input:text').val('');
+      return $('#song-recs p').text("Thanks for recommending " + song + ". We'll think about it!");
     });
   });
 
